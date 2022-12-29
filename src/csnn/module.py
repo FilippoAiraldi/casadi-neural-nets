2c0878d7b81ce1c1cc56c5fdfeafb1c52ce8b9f2
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-12-29 19:15:36 +0100

    implement repr for module

--- src/csnn/module.py
+++ src/csnn/module.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections import OrderedDict
 from functools import singledispatchmethod
 from typing import Any, Dict, Generic, Iterator, Optional, Tuple, TypeVar
 
@@ -10,6 +11,17 @@ T = TypeVar("T", bound="Module")
 SymType = TypeVar("SymType", cs.SX, cs.MX)
 
 
+def _addindent(s_, numSpaces):
+    s = s_.split("\n")
+    # don't do anything for single-line stuff
+    if len(s) == 1:
+        return s_
+    first = s.pop(0)
+    s = [(numSpaces * " ") + line for line in s]
+    s = "\n".join(s)
+    return first + "\n" + s
+
+
 class Module(ABC, Generic[SymType]):
     """Base class for all neural network modules. Your models should also subclass this
     class."""
@@ -17,9 +29,9 @@ class Module(ABC, Generic[SymType]):
     def __init__(self) -> None:
         """Initializes the module."""
         self.training: bool = False
-        self._sym_parameters: Dict[str, SymType] = {}
-        self._num_parameters: Dict[str, npt.NDArray[np.double]] = {}
-        self._modules: Dict[str, "Module"] = {}
+        self._sym_parameters: Dict[str, SymType] = OrderedDict()
+        self._num_parameters: Dict[str, npt.NDArray[np.double]] = OrderedDict()
+        self._modules: Dict[str, "Module"] = OrderedDict()
 
     def register_parameter(
         self, name: str, sym: SymType, val: Optional[npt.NDArray[np.double]] = None
@@ -132,7 +144,7 @@ class Module(ABC, Generic[SymType]):
                 yield from module.num_parameters(recurse=True, prefix=f"{prefix}{name}")
 
     @abstractmethod
-    def forward_sym(self, x: SymType) -> SymType:
+    def forward_sym(self, input: SymType) -> SymType:
         """Forwards symbolically the given input through the neural net.
 
         Parameters
@@ -147,7 +159,7 @@ class Module(ABC, Generic[SymType]):
         """
 
     @abstractmethod
-    def forward_num(self, x: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
+    def forward_num(self, input: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
         """Forwards numerically the given input through the neural net.
 
         Parameters
@@ -198,6 +210,10 @@ class Module(ABC, Generic[SymType]):
         """
         return self.train(False)
 
+    def extra_repr(self) -> str:
+        """Sets the extra representation of the module."""
+        return ""
+
     @singledispatchmethod
     def __call__(self, x: SymType) -> SymType:
         return self.forward_sym(x)
@@ -219,3 +235,24 @@ class Module(ABC, Generic[SymType]):
         ):
             self.register_parameter(name, *value)
         return super().__setattr__(name, value)
+
+    def __repr__(self):
+        # We treat the extra repr like the sub-module, one item per line
+        extra_lines = []
+        # empty string will be split into list ['']
+        if extra_repr := self.extra_repr():
+            extra_lines = extra_repr.split("\n")
+        child_lines = []
+        for key, module in self._modules.items():
+            mod_str = repr(module)
+            mod_str = _addindent(mod_str, 2)
+            child_lines.append(f"({key}): {mod_str}")
+        main_str = f"{self.__class__.__name__}("
+        if lines := extra_lines + child_lines:
+            # simple one-liner info, which most builtin Modules will use
+            if len(extra_lines) == 1 and not child_lines:
+                main_str += extra_lines[0]
+            else:
+                main_str += "\n  " + "\n  ".join(lines) + "\n"
+        main_str += ")"
+        return main_str
