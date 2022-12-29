8798b63d9722e3edc7d9fb72ab3798bafeb05af4
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-12-29 12:19:20 +0100

    implementing base Module

--- src/csnn/module.py
+++ src/csnn/module.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from functools import singledispatchmethod
-from typing import Dict, Generic, TypeVar
+from typing import Dict, Generic, Iterator, Optional, Tuple, TypeVar
 
 import casadi as cs
 import numpy as np
@@ -10,18 +10,145 @@ SymType = TypeVar("SymType", cs.SX, cs.MX)
 
 
 class Module(ABC, Generic[SymType]):
+    """Base class for all neural network modules. Your models should also subclass this
+    class."""
+
     def __init__(self) -> None:
+        """Initializes the module."""
         self.training: bool = False
         self._sym_parameters: Dict[str, SymType] = {}
+        self._num_parameters: Dict[str, npt.NDArray[np.double]] = {}
         self._modules: Dict[str, "Module"] = {}
+
+    def register_parameter(
+        self, name: str, sym: SymType, val: Optional[npt.NDArray[np.double]] = None
+    ) -> None:
+        """Adds a parameter to the module.
+
+        Parameters
+        ----------
+        name : str
+            Name of the parameter.
+        sym : SymType
+            Symbol of the parameter.
+        val : numpy array, optional
+            Numerical value of the parameter. If `None`, an empty array is instanciated.
+
+        Raises
+        ------
+        KeyError
+            Raises if `name` is already in use.
+        """
+        if name in self._sym_parameters:
+            raise KeyError(f"Parameter {name} already exists.")
+        if val is None:
+            val = np.empty(sym.shape, dtype=float)
+        assert val.shape == sym.shape, "Incompatible shapes."
+        self._sym_parameters[name] = sym
+        self._num_parameters[name] = val
+
+    def add_module(self, name: str, module: "Module") -> None:
+        """Adds a child module to the current module.
+
+        Parameters
+        ----------
+        name : str
+            Name of the child module
+        module : Module
+            Child module to be added to this module.
+
+        Raises
+        ------
+        KeyError
+            Raises if `name` is already in use.
+        """
+        if name in self._modules:
+            raise KeyError(f"Child module {name} already exists.")
+        self._modules[name] = module
+
+    def sym_parameters(
+        self, recurse: bool = True, prefix: str = ""
+    ) -> Iterator[Tuple[str, SymType]]:
+        """Returns an iterator over the module's parameters.
+
+        Parameters
+        ----------
+        recurse : bool, optional
+            If `True`, then yields parameters of this module and all submodules.
+            Otherwise, yields only parameters that are direct members of this module. By
+            default `True`.
+        prefix : str, optional
+            Prefix to add in front of this module's name.
+
+        Yields
+        ------
+        Iterator of tuple[str, casadi.SX or MX]
+            An iterator over tuples of parameter's names and symbols.
+        """
+        if prefix != "":
+            prefix += "."
+        for name, par in self._sym_parameters.items():
+            yield (prefix + name, par)
+        if recurse:
+            for name, module in self._modules.items():
+                yield from module.sym_parameters(recurse=True, prefix=f"{prefix}{name}")
+
+    def num_parameters(
+        self, recurse: bool = True, prefix: str = ""
+    ) -> Iterator[Tuple[str, Optional[npt.NDArray[np.double]]]]:
+        """Returns an iterator over the module's parameters.
+
+        Parameters
+        ----------
+        recurse : bool, optional
+            If `True`, then yields parameters of this module and all submodules.
+            Otherwise, yields only parameters that are direct members of this module. By
+            default `True`.
+        prefix : str, optional
+            Prefix to add in front of this module's name.
+
+        Yields
+        ------
+        Iterator of tuple[str, casadi.SX or MX]
+            An iterator over tuples of parameter's names and numerical values.
+        """
+        if prefix != "":
+            prefix += "."
+        for name, par in self._num_parameters.items():
+            yield (prefix + name, par)
+        if recurse:
+            for name, module in self._modules.items():
+                yield from module.num_parameters(recurse=True, prefix=f"{prefix}{name}")
 
     @abstractmethod
     def forward_sym(self, x: SymType) -> SymType:
-        pass
+        """Forwards symbolically the given input through the neural net.
+
+        Parameters
+        ----------
+        x : SymType
+            Symbolical input.
+
+        Returns
+        -------
+        SymType
+            The symbolical output of the net.
+        """
 
     @abstractmethod
-    def forward_np(self, x: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
-        pass
+    def forward_num(self, x: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
+        """Forwards numerically the given input through the neural net.
+
+        Parameters
+        ----------
+        x : numpy array
+            Symbolical input.
+
+        Returns
+        -------
+        numpy array
+            The numerical output of the net.
+        """
 
     @singledispatchmethod
     def __call__(self, x: SymType) -> SymType:
@@ -29,4 +156,7 @@ class Module(ABC, Generic[SymType]):
 
     @__call__.register  # type: ignore
     def _(self, x: np.ndarray) -> np.ndarray:
-        return self.forward_np(x)
+        return self.forward_num(x)
+
+
+# TODO: eval and train
--- tests/test_module.py
+++ tests/test_module.py
@@ -14,7 +14,7 @@ class DummyModule(Module):
     def forward_sym(self, x: SymType) -> SymType:
         return (cs.SX if isinstance(x, cs.SX) else cs.MX).zeros(2, 2)
 
-    def forward_np(self, x: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
+    def forward_num(self, x: npt.NDArray[np.double]) -> npt.NDArray[np.double]:
         return np.random.rand(2, 2)
 
 
@@ -24,6 +24,54 @@ class TestModule(unittest.TestCase):
         for x in [cs.SX.zeros(2, 2), cs.MX.zeros(2, 2), np.zeros((2, 2))]:
             y = module(x)
             self.assertEqual(type(x), type(y))
+
+    def test_register_parameter__raises__with_used_name(self):
+        module = DummyModule()
+        module.register_parameter("ciao", cs.SX.zeros(2, 2))
+        with self.assertRaisesRegex(KeyError, "Parameter ciao already exists."):
+            module.register_parameter("ciao", cs.SX.zeros(2, 2))
+
+    def test_register_parameter__instantiates_empty_numerical_array(self):
+        module = DummyModule()
+        shape = (2, 3)
+        module.register_parameter("ciao", cs.SX.zeros(*shape))
+        self.assertIn("ciao", module._num_parameters)
+        self.assertIsInstance(module._num_parameters["ciao"], np.ndarray)
+        self.assertEqual(module._num_parameters["ciao"].shape, shape)
+
+    def test_register_parameter__raises__with_invalid_shape(self):
+        module = DummyModule()
+        with self.assertRaisesRegex(AssertionError, "Incompatible shapes."):
+            module.register_parameter("ciao", cs.SX.zeros(2, 2), np.random.rand(2, 3))
+
+    def test_add_module__raises__with_used_name(self):
+        module = DummyModule()
+        module.add_module("ciao", DummyModule())
+        with self.assertRaisesRegex(KeyError, "Child module ciao already exists."):
+            module.add_module("ciao", DummyModule())
+
+    def test_add_module__adds_module_correctly(self):
+        module = DummyModule()
+        module.add_module("ciao", DummyModule())
+        self.assertIn("ciao", module._modules)
+
+    def test_sym_num_parameters__returns_all_parameters(self):
+        # sourcery skip: extract-duplicate-method
+        module = DummyModule()
+        p1 = cs.SX.zeros(2, 1)
+        module.register_parameter("p1", p1)
+        child_module = DummyModule()
+        p2 = cs.SX.zeros(2, 4)
+        child_module.register_parameter("p2", p2)
+        module.add_module("child", child_module)
+
+        L1 = list(module.sym_parameters())
+        L2 = list(module.num_parameters())
+
+        self.assertEqual(sum(n == "p1" for n, _ in L1), 1)
+        self.assertEqual(sum(n == "child.p2" for n, _ in L1), 1)
+        self.assertEqual(sum(n == "p1" for n, _ in L2), 1)
+        self.assertEqual(sum(n == "child.p2" for n, _ in L2), 1)
 
 
 if __name__ == "__main__":
